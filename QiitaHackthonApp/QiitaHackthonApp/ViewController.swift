b88c274cadeb45f8f167d2ee3729042754058eef
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2024-01-19 12:31:08 +0900

    chore: create new workspace.

---
+++ QiitaHackthonApp/QiitaHackthonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QiitaHackthonApp
+//
+//  Created by 松浦裕久 on 2024/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QiitaHackthonApp/QiitaHackthonAppUITests/QiitaHackthonAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QiitaHackthonAppUITestsLaunchTests.swift
+//  QiitaHackthonAppUITests
+//
+//  Created by 松浦裕久 on 2024/01/19.
+//
+
+import XCTest
+
+final class QiitaHackthonAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
