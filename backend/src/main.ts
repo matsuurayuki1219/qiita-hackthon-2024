4411a8a0f287f65037cd237f0aef71525e4e296e
KokiKono <kono.koki.pg@gmail.com>
2024-02-10 15:48:36 +0900

    chore: Setup swagger ui

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Qiita Hackthon 2024')
+    .setDescription('The API description')
+    .setVersion('0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
